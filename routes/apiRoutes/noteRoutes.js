05f1024ea52c1060462bb5d5c7c09db87b7cee5e
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-08-07 15:36:29 -0500

    notes populate that are saved

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,67 +1,21 @@
 const router = require('express').Router();
-const fs = require('fs');
-var uniqid = require('uniqid'); 
+const createNewNote = require('../../lib/notes');
+const noteData = require('../../db/db.json')
 
 
 router.get('/notes',(req,res) => {
-    // send message to client
-    res.json(`${req.method} request recieved to get notes`);
-
-
+    res.json(noteData);
+    
     // Log request to terminal 
     console.info(`${req.method} request recieved to get notes`)
 })
 
 router.post('/notes', (req, res) => {
     console.info(`${req.method} request recieved to add/save notes`);
-
-    const {title, text} = req.body;
-
-      // If all the required properties are present
-    if (title && text) {
-        // Variable for the object we will save
-        const newNote = {
-        title,
-        text,
-        note_id: uniqid(),
-        };
-
-   // Obtain existing notes
-   fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      // Convert string into JSON object
-      const parsedNotes = JSON.parse(data);
-
-      // Add a new note
-      parsedNotes.push(newNote);
-
-      // Write updated reviews back to the file
-      fs.writeFile(
-        './db/db.json',
-        JSON.stringify(parsedNotes, null, 4),
-        (writeErr) =>
-          writeErr
-            ? console.error(writeErr)
-            : console.info('Successfully updated notes!')
-      );
-    }
-  });
-
-  const response = {
-    status: 'success',
-    body: newNote,
-  };
-
-  console.log(response);
-  res.json(response);
-} else {
-  res.json('Error in posting review');
-}
+    const note = createNewNote(req.body);
+    res.json(note);
+    
 });
 
 
-
-  
 module.exports = router; 
